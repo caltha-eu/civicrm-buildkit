18a81de55d1d6345f3667bfde2e4626a82f8a897
Michal Mach <michal@mach.org.pl>
2017-05-09 12:14:11 +0200

    some more fixes to the template

--- app/config/drupal-caltha/uninstall.sh
+++ app/config/drupal-caltha/uninstall.sh
@@ -4,5 +4,10 @@
 
 ###############################################################################
 
+if [ -f "$CIVI_SETTINGS" ]; then
+  chmod u+w $(dirname "$CIVI_SETTINGS")
+  rm -f "$CIVI_SETTINGS"
+fi
+
 drupal_uninstall
 amp_uninstall
